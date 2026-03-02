743981c18c58906e88cf7ecc20374aa1a79a56de
Asan <Asan@DESKTOP-A5E79TK>
2026-03-02 20:49:10 +0200

    Created Db context, added appropriate packages for each layer, configured connection string

---
+++ BoardGameLogger.Data/BoardGameLoggerDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BoardGameLogger.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameLogger.Data
+{
+    public class BoardGameLoggerDbContext : DbContext
+    {
+        public BoardGameLoggerDbContext(DbContextOptions<BoardGameLoggerDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<BoardGame> BoardGames { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<LoanLog> LoanLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+}
--- BoardGameLogger/Program.cs
+++ BoardGameLogger/Program.cs
@@ -1,3 +1,6 @@
+using BoardGameLogger.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<BoardGameLoggerDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
